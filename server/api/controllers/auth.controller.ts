e063ff2456a84b11f83eec522817f2a55559c581
Shubham Singh <shubhamsingh@SHUBHAMs-MacBook-Pro.local>
2020-06-01 22:29:01 +0530

    updating the install-dev script

--- server/api/controllers/auth.controller.ts
+++ server/api/controllers/auth.controller.ts
@@ -1,8 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
+import { AuthService } from '../services';
+
+// Create Authentication Service instance
+const authService = new AuthService();
 
 export class AuthController {
 
     async signIn(req: Request, res: Response, next: NextFunction){
-        
+        try {
+
+            // Fetch the data variables from the request
+            let { body: { user } } = req;
+
+            // Call the signIn function from the service
+            let signIn = await authService.signIn(user.email, user.password)
+            
+            // Process with the response
+            .then(()=>{
+                return res.status(200).json({
+                    message: 'User has been signedIn!',
+                    
+                })
+            })
+
+            // Catch the errors from the service
+            .catch((err)=>{
+                return res.status(400).json({
+                    message: 'Bad Request, kindly trace the error stack for more details!',
+                    error: new Error(err)
+                })
+            })
+
+
+        } catch(err){
+            return res.status(500).json({
+                message: 'Internal Server Error!',
+                error: new Error(err)
+            })
+        }
     }
 }
--- server/api/services/auth.service.ts
+++ server/api/services/auth.service.ts
@@ -2,8 +2,16 @@ import { Auth, User } from '../models';
 
 export class AuthService {
 
-    async signIn(){
+    async signIn(email: string, password: string){
 
+    }
+
+    async signUp(first_name: string, last_name: string, email: string, password: string){
+
+    }
+
+    async signout(){
+        
     }
 
 }
