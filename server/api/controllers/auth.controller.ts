280bb422494226b7689e3d229b5ba4061a310af2
Shubham Singh <shubhamsingh@SHUBHAMs-MacBook-Pro.local>
2020-06-01 22:49:40 +0530

    adding controller states

--- server/api/controllers/auth.controller.ts
+++ server/api/controllers/auth.controller.ts
@@ -6,33 +6,100 @@ const authService = new AuthService();
 
 export class AuthController {
 
-    async signIn(req: Request, res: Response, next: NextFunction){
+    async signIn(req: Request, res: Response, next: NextFunction) {
         try {
 
             // Fetch the data variables from the request
             let { body: { user } } = req;
 
             // Call the signIn function from the service
-            let signIn = await authService.signIn(user.email, user.password)
-            
-            // Process with the response
-            .then(()=>{
-                return res.status(200).json({
-                    message: 'User has been signedIn!',
-                    
+            user = await authService.signIn(user.email, user.password)
+
+                // Process with the status 200 response
+                .then(() => {
+                    return res.status(200).json({
+                        message: 'User has been signed in successfully!',
+                        user: user
+                    })
                 })
-            })
 
-            // Catch the errors from the service
-            .catch((err)=>{
-                return res.status(400).json({
-                    message: 'Bad Request, kindly trace the error stack for more details!',
-                    error: new Error(err)
+                // Catch the errors from the service function
+                .catch((err) => {
+                    return res.status(400).json({
+                        message: 'Bad Request, kindly trace the error stack for more details!',
+                        error: new Error(err)
+                    })
                 })
+
+
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error!',
+                error: new Error(err)
             })
+        }
+    }
+
+    async signUp(req: Request, res: Response, next: NextFunction) {
+        try {
+
+            // Fetch the data variables from the request
+            let { body: { user } } = req;
+
+            // Call the signUp function from the service
+            user = await authService.signUp(user.first_name, user.last_name, user.email, user.password)
+
+                // Process with the status 200 response
+                .then(() => {
+                    return res.status(200).json({
+                        message: 'User has been signed up successfully!',
+                        user: user
+                    })
+                })
+
+                // Catch the errors from the service function
+                .catch((err) => {
+                    return res.status(400).json({
+                        message: 'Bad Request, kindly trace the error stack for more details!',
+                        error: new Error(err)
+                    })
+                })
 
 
-        } catch(err){
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error!',
+                error: new Error(err)
+            })
+        }
+    }
+
+    async signOut(req: Request, res: Response, next: NextFunction) {
+        try {
+
+            // Fetch the userId from the server's cache
+            let userId = req['userId'];
+
+            // Call the sign out service function
+            let user = await authService.signOut(userId)
+
+                // Process with the status 200 response
+                .then(() => {
+                    return res.status(200).json({
+                        message: 'User has been signed out successfully!',
+                        user: user
+                    })
+                })
+
+                // Catch the errors from the service function
+                .catch((err) => {
+                    return res.status(400).json({
+                        message: 'Bad Request, kindly trace the error stack for more details!',
+                        error: new Error(err)
+                    })
+                })
+
+        } catch (err) {
             return res.status(500).json({
                 message: 'Internal Server Error!',
                 error: new Error(err)
