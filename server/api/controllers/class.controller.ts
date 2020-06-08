950bfe77db76edf637fec45dacc740f462c19eb4
krrishdholakia <krrishdholakia@gmail.com>
2020-06-09 01:23:19 +0530

    join class changes

--- server/api/controllers/class.controller.ts
+++ server/api/controllers/class.controller.ts
@@ -1,131 +1,170 @@
-import { Request, Response, NextFunction } from 'express'
-import { ClassService } from '../services'
+import { Request, Response, NextFunction } from "express";
+import { ClassService } from "../services";
 
 // Create Authentication Service instance
-const classService = new ClassService()
+const classService = new ClassService();
 
 export class ClassController {
-
   /**
    * This function is responsible for creation a class
-   * @param { headers: { authorization } }req 
-   * @param res 
-   * @param next 
+   * @param { headers: { authorization } }req
+   * @param res
+   * @param next
    */
   async createClass(req: Request, res: Response, next: NextFunction) {
     try {
-
       // Fetch the headers from the request
-      let { headers: { authorization } } = req
+      let {
+        headers: { authorization },
+      } = req;
 
       // Call the create class service function
-      await classService
-        .createClass(authorization)
-        .then((response) => {
-          return res.status(200).json({
-            message: 'User has successfully created a class!',
-            class: response.class
-          })
-        })
-
+      await classService.createClass(authorization).then((response) => {
+        return res.status(200).json({
+          message: "User has successfully created a class!",
+          class: response.class,
+        });
+      });
     } catch (err) {
       return res.status(500).json({
-        message: 'Internal Server Error!',
-        error: new Error(err || ' Internal Server Error'),
-      })
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
+    }
+  }
+
+  async inviteToClass(req: Request, res: Response, next: NextFunction) {
+    try {
+      //fetch authorization from header of request
+      let {
+        headers: { authorization },
+        body: { studentEmails, classId },
+      } = req;
+
+      //call invite to class service function
+      await classService
+        .inviteToClass(authorization, studentEmails, classId)
+        .then((response) => {
+          return res.status(200).json({
+            message: "User has successfully invited to class!",
+          });
+        });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
+    }
+  }
+
+  async joinClass(req: Request, res: Response, next: NextFunction) {
+    try {
+      // fetch authorization and class to be joined
+      let {
+        headers: { authorization },
+        body: { classId },
+      } = req;
+
+      //call join class service function
+      await classService.joinClass(classId, authorization).then(() => {
+        return res.status(200).json({
+          message: "User has successfully joined class!",
+        });
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
     }
   }
 
   /**
    * This function is responsible for fetching the class details
-   * @param { query: { classId } }req 
-   * @param res 
-   * @param next 
+   * @param { query: { classId } }req
+   * @param res
+   * @param next
    */
   async getClassDetails(req: Request, res: Response, next: NextFunction) {
     try {
-
       // Fetch the classId from the request
-      let { query: { classId } } = req
+      let {
+        query: { classId },
+      } = req;
 
       // Call the get class details service function
       await classService
         .getClassDetails(classId.toString())
         .then((class_details) => {
           return res.status(200).json({
-            message: 'Class has been successfully found!',
+            message: "Class has been successfully found!",
             class: class_details,
-          })
-        })
-
+          });
+        });
     } catch (err) {
       return res.status(500).json({
-        message: 'Internal Server Error!',
-        error: new Error(err || ' Internal Server Error'),
-      })
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
     }
   }
 
   /**
    * This function is responsible for removing a class
-   * @param { body: { jagrik_class }, headers: { authorization } } req 
-   * @param res 
-   * @param next 
+   * @param { body: { jagrik_class }, headers: { authorization } } req
+   * @param res
+   * @param next
    */
   async deleteClass(req: Request, res: Response, next: NextFunction) {
     try {
-
       // Fetch the required contents from the request
       let {
         params: { classId },
-        headers: { authorization }
-      } = req
+        headers: { authorization },
+      } = req;
 
       // Call the delete class service function
       await classService
         .deleteClass(authorization, classId)
         .then((class_details) => {
           return res.status(200).json({
-            message: 'Class has been successfully deleted!',
+            message: "Class has been successfully deleted!",
             class: class_details,
-          })
-        })
-
+          });
+        });
     } catch (err) {
       return res.status(500).json({
-        message: 'Internal Server Error!',
-        error: new Error(err || ' Internal Server Error'),
-      })
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
     }
   }
 
   /**
    * This function is responisble for fetching the classes list for currently loggedIn user
-   * @param { headers: { authorization } } req 
-   * @param res 
-   * @param next 
+   * @param { headers: { authorization } } req
+   * @param res
+   * @param next
    */
   async getUserClasses(req: Request, res: Response, next: NextFunction) {
     try {
-
       // Fetch the authorization header from the request
-      let { headers: { authorization } } = req
+      let {
+        headers: { authorization },
+      } = req;
 
       // Call the service function to get all the classes
-      await classService
-        .getUserClasses(authorization)
-        .then((classes) => {
-          return res.status(200).json({
-            message: 'Classes have been successfully found!',
-            classes: classes,
-          })
-        })
-
+      await classService.getUserClasses(authorization).then((classes) => {
+        return res.status(200).json({
+          message: "Classes have been successfully found!",
+          classes: classes,
+        });
+      });
     } catch (err) {
       return res.status(500).json({
-        message: 'Internal Server Error!',
-        error: new Error(err || ' Internal Server Error'),
-      })
+        message: "Internal Server Error!",
+        error: new Error(err || " Internal Server Error"),
+      });
     }
   }
 }
--- server/api/routes/class.routes.ts
+++ server/api/routes/class.routes.ts
@@ -1,38 +1,49 @@
-import express from 'express'
-import { ClassController } from '../controllers'
+import express from "express";
+import { ClassController } from "../controllers";
 
 // Auth Controller with all the functions
-const classController = new ClassController()
+const classController = new ClassController();
 
 // Routes List
-const routes = express.Router()
+const routes = express.Router();
 
 /**
  * POST - Creates a new jagrik class
  * @var { headers: { authorization } }
  */
-routes.post('/', classController.createClass)
+routes.post("/", classController.createClass);
 
 /**
  * GET - Get class details based on classId(query paramater)
  * @var { query: { classId } }
  */
-routes.get('/', classController.getClassDetails)
+routes.get("/", classController.getClassDetails);
 
 /**
  * DELETE - Delete a class
  * @var { params: { classId }, headers: { authorization } }
  */
-routes.delete('/:classId', classController.deleteClass)
+routes.delete("/:classId", classController.deleteClass);
 
 /**
  * GET - Get all classes a user is in
  * @var { headers: { authorization } }
  */
-routes.get('/user', classController.getUserClasses)
+routes.post("/user", classController.getUserClasses);
 
+/**
+ * POST - Invites users to class
+ * @var { headers: { authorization }, body: {studentEmails, classId} }
+ */
+routes.post("/invite-to-class", classController.inviteToClass);
+
+/**
+ * POST - Allow user to join class
+ * @var { headers: { authorization }, body: {classId} }
+ */
+routes.post("/join-class", classController.joinClass);
 /*  ===================
  *  -- EXPORT ROUTES --
  *  ===================
  * */
-export { routes as classRoutes }
+export { routes as classRoutes };
