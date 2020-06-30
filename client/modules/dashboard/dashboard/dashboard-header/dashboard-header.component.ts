92890ea5c912bba69533e99449c2351b697e9c99
Vinay Gupta <sunriseappsinc@gmail.com>
2020-07-01 02:58:22 +0530

    added material ui

--- client/modules/dashboard/dashboard/dashboard-header/dashboard-header.component.ts
+++ client/modules/dashboard/dashboard/dashboard-header/dashboard-header.component.ts
@@ -7,69 +7,85 @@ import { UtilityService } from 'src/shared/services/utility-service/utility.serv
 @Component({
   selector: 'app-dashboard-header',
   templateUrl: './dashboard-header.component.html',
-  styleUrls: ['./dashboard-header.component.scss']
+  styleUrls: ['./dashboard-header.component.scss'],
 })
 export class DashboardHeaderComponent implements OnInit {
+  constructor(private _Injector: Injector, private _Router: Router) {}
 
-  constructor(
-    private _Injector: Injector,
-    private _Router: Router
-  ) { }
+  showFiller = false;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   navigateToUserProfile() {
-
     // Storage Service instance
-    let storageService = this._Injector.get(StorageService)
+    let storageService = this._Injector.get(StorageService);
 
     // Fetch the user Id
-    let userId = storageService.getLocalData('userData')._id
+    let userId = storageService.getLocalData('userData')._id;
 
     // Navigate to router
     this._Router.navigate(['/dashboard', 'user'], {
       queryParams: {
-        userId: userId
-      }
-    })
+        userId: userId,
+      },
+    });
   }
 
   signOut() {
-
     return new Promise((resolve) => {
-
       // Creating auth service instance
-      let authService = this._Injector.get(AuthenticationService)
+      let authService = this._Injector.get(AuthenticationService);
 
       // Storage Service instance
-      let storageService = this._Injector.get(StorageService)
+      let storageService = this._Injector.get(StorageService);
 
       // Utility Service Instance
-      let utilityService = this._Injector.get(UtilityService)
+      let utilityService = this._Injector.get(UtilityService);
 
       // Call the service function
-      authService.signOut(storageService.getLocalData('userData')._id)
+      authService
+        .signOut(storageService.getLocalData('userData')._id)
         .then((res) => {
-
           // Resolve with sucess
-          resolve(res)
+          resolve(res);
 
           // Navigate the User to home page
-          this._Router.navigate([''])
+          this._Router.navigate(['']);
 
           // Fire sucess toast
-          utilityService.fireToast('success', `${storageService.getLocalData('userData').first_name}, you have been logged out sucessfully!`)
+          utilityService.fireToast(
+            'success',
+            `${
+              storageService.getLocalData('userData').first_name
+            }, you have been logged out sucessfully!`
+          );
 
           // Store the token locally
-          storageService.clear()
+          storageService.clear();
         })
         .catch(() => {
-
           // Fire error toast
-          utilityService.fireToast('error', `Some unexpected error occured, please try again!`)
-        })
-    })
+          utilityService.fireToast(
+            'error',
+            `Some unexpected error occured, please try again!`
+          );
+        });
+    });
   }
 
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
 }
