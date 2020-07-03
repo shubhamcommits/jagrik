1406edb485564ceb0ec14c1d0ff94adb2d7eec1a
Shubham Singh <shubhamsingh@SHUBHAMs-MacBook-Pro.local>
2020-07-04 02:39:56 +0530

    adding teams creation

--- server/api/models/index.ts
+++ server/api/models/index.ts
@@ -1,12 +1,9 @@
 import { Auth } from './auth.model'
 import { Card } from './card.model'
 import { Class } from './class.model'
-<<<<<<< HEAD
 import { Team } from './team.model'
-=======
 import { Task } from './task.model'
 import { User } from './user.model'
->>>>>>> 14b61c6... task in progress
 
 /*  =====================
  *  -- EXPORTS MODELS --
@@ -23,14 +20,11 @@ export {
   // Class Model
   Class,
 
-<<<<<<< HEAD
   Team,
-=======
   // Task Model
   Task,
 
   // User Model
   User
->>>>>>> 14b61c6... task in progress
 
 }
