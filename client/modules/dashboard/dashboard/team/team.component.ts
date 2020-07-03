907025a02c9cca3854ff9066b204f6f395dd9866
Vinay Gupta <sunriseappsinc@gmail.com>
2020-07-04 02:22:33 +0530

    added apis of team creation and listing

--- client/modules/dashboard/dashboard/team/team.component.ts
+++ client/modules/dashboard/dashboard/team/team.component.ts
@@ -3,25 +3,18 @@ import { TeamService } from '../shared/services/team.service';
 import { UtilityService } from 'src/shared/services/utility-service/utility.service';
 import { StorageService } from 'src/shared/services/storage-service/storage.service';
 
-export interface PeriodicElement {
+export interface TeamElement {
   name: string;
   position: number;
-  className: number;
+  className: String;
   team: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', className: 1.0079, team: 'H' },
-  { position: 2, name: 'Helium', className: 4.0026, team: 'He' },
-  { position: 3, name: 'Lithium', className: 6.941, team: 'Li' },
-  { position: 4, name: 'Beryllium', className: 9.0122, team: 'Be' },
-  { position: 5, name: 'Boron', className: 10.811, team: 'B' },
-  { position: 6, name: 'Carbon', className: 12.0107, team: 'C' },
-  { position: 7, name: 'Nitrogen', className: 14.0067, team: 'N' },
-  { position: 8, name: 'Oxygen', className: 15.9994, team: 'O' },
-  { position: 9, name: 'Fluorine', className: 18.9984, team: 'F' },
-  { position: 10, name: 'Neon', className: 20.1797, team: 'Ne' },
-];
+export interface AssignTeamElement {
+  name: string;
+  _id: String;
+  className: String;
+}
 
 
 @Component({
@@ -38,8 +31,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class TeamComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'name', 'className', 'team'];
-  dataSource = ELEMENT_DATA;
+  dataSource: TeamElement[] = [];
+  newUser: AssignTeamElement[] = [];
   userRole = ''
+
 
   constructor(private teamService: TeamService, private utilityService: UtilityService, private storageService: StorageService) { }
 
@@ -48,13 +43,52 @@ export class TeamComponent implements OnInit {
     this.getTeams()
   }
 
-  assignTeam() {
-
+  assignTeam(userId) {
+    this.teamService.createTeam(this.storageService.getLocalData('userData').classes[0], userId).then((res) => {
+      console.log(res)
+      this.getTeams()
+      this.utilityService.fireToast('success', `Successfully Assigned Team`)
+    }).catch(() => {
+      // Fire error toast
+      this.utilityService.fireToast('error', `Some unexpected error occured, please try again!`)
+    })
   }
 
   getTeams() {
 
-    this.teamService.getTeams(this.storageService.getLocalData('userData').classes[0])
+    this.teamService.getTeams(this.storageService.getLocalData('userData').classes[0]).then((res) => {
+      console.log(res)
+      let data: any = res['teams'];
+      if (data['members'].length > 0) {
+        var i = 0;
+        this.dataSource = [];
+        this.newUser = [];
+        data['members'].forEach(element => {
+
+          if ("0" in element['teams'] == true) {
+            this.dataSource.push({
+              name: `${element['first_name']} ${element['last_name']}`,
+              position: i++,
+              className: data['name'],
+              team: element['teams']['0'],
+            })
+          } else {
+            this.newUser.push({
+              name: `${element['first_name']} ${element['last_name']}`,
+              _id: element['_id'],
+              className: data['name']
+            })
+          }
+
+        });
+
+      }
+
+    })
+      .catch(() => {
+        // Fire error toast
+        this.utilityService.fireToast('error', `Some unexpected error occured, please try again!`)
+      })
   }
 
 }
