20ffddef6e8487939b301237631e32483cfa756f
krrishdholakia <krrishdholakia@gmail.com>
2020-07-05 17:29:47 +0530

    react-vid-chat

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// Make a copy of this file and save it as config.js (in the src directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL = "http://YOUR-SERVER-URL";
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+const API_KEY = "46824144";
+const SESSION_ID =
+  "2_MX40NjgyNDE0NH5-MTU5Mzg2MDU4MDAxNX5UT1FNZmVnaHIyKzR4MVVUTEtiblVlVDd-fg";
+const TOKEN =
+  "T1==cGFydG5lcl9pZD00NjgyNDE0NCZzaWc9N2U1YzY4NTQzMDljMDk2MDlmMWQwNDcxYjE0NzYxYjYwMGU5NmUxNjpzZXNzaW9uX2lkPTJfTVg0ME5qZ3lOREUwTkg1LU1UVTVNemcyTURVNE1EQXhOWDVVVDFGTlptVm5hSEl5S3pSNE1WVlVURXRpYmxWbFZEZC1mZyZjcmVhdGVfdGltZT0xNTkzODYwNTgwJnJvbGU9cHVibGlzaGVyJm5vbmNlPTE1OTM4NjA1ODAuMDU2NTExNTQ3MjMyNDE=";
+
+export { SAMPLE_SERVER_BASE_URL, API_KEY, SESSION_ID, TOKEN };
