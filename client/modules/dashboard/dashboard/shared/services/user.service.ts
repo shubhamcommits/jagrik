2bcad4e8268ff3b74b4f944b989f578c790eb6bb
Vinay Gupta <vinay.gupta@un.org>
2020-07-14 23:33:04 +0530

    added changes of profile api and validation

--- client/modules/dashboard/dashboard/shared/services/user.service.ts
+++ client/modules/dashboard/dashboard/shared/services/user.service.ts
@@ -3,28 +3,39 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   /**
    * This function is responsible for assigning a card to the user
-   * @param theme 
-   * @param week 
+   * @param theme
+   * @param week
    */
   assignCard(theme: String, week: Number) {
-    return this.httpClient.post(environment.baseApiUrl + '/users/assign-card', {
-      card_theme: theme,
-      week: week
-    }).toPromise()
+    return this.httpClient
+      .post(environment.baseApiUrl + '/users/assign-card', {
+        card_theme: theme,
+        week: week,
+      })
+      .toPromise();
+  }
+
+  /**
+   * This function is responsible for update user profile
+   * @param user
+   */
+  uodateProfile(userData: any) {
+    return this.httpClient
+      .put(environment.baseApiUrl + '/users', { user : userData})
+      .toPromise();
   }
 
   /**
    * This function is responsible for fetching the current user's details
    */
-  get(){
-    return this.httpClient.get(environment.baseApiUrl + '/users').toPromise()
+  get() {
+    return this.httpClient.get(environment.baseApiUrl + '/users').toPromise();
   }
 }
