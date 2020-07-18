6d76708a7b23a71568c56db70eec9a98c7521648
Vinay Gupta <vinaygupta771@gmail.com>
2020-07-18 20:43:02 +0530

    added changes

--- client/modules/dashboard/dashboard/dashboard-inbox/facilitator-view/facilitator-view.component.ts
+++ client/modules/dashboard/dashboard/dashboard-inbox/facilitator-view/facilitator-view.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../../shared/services/class.service';
+import { StorageService } from 'src/shared/services/storage-service/storage.service';
+import { UtilityService } from 'src/shared/services/utility-service/utility.service';
 
+export interface TeamElement {
+  team_id: string;
+  team_name: String;
+  team_status: String;
+}
 @Component({
   selector: 'app-facilitator-view',
   templateUrl: './facilitator-view.component.html',
-  styleUrls: ['./facilitator-view.component.scss']
+  styleUrls: ['./facilitator-view.component.scss'],
+  styles: [
+    `
+      :host {
+        display: inline-block;
+        width: 100%;
+      }
+    `,
+  ],
 })
 export class FacilitatorViewComponent implements OnInit {
+  constructor(
+    private classService: ClassService,
+    private storageService: StorageService,
+    private utilityService: UtilityService
+  ) {}
 
-  constructor() { }
+  is_completedTask = false;
+  displayedColumns: string[] = ['team_id', 'team_name', 'team_status'];
+  dataSource: TeamElement[] = [];
 
   ngOnInit(): void {
+    this.getCompletedTeam(
+      this.storageService.getLocalData('userData').classes[0]
+    );
   }
 
+  getCompletedTeam(classId) {
+    return new Promise((resolve) => {
+      // Call the service function
+      this.classService
+        .getCompletedTaskTeam(classId)
+        .then((res) => {
+          var data: any = res;
+          data.tasks.forEach(element => {
+            if (element['team_status'] === 'completed'){
+            this.dataSource.push({
+              team_id:
+                element['team_id'],
+              team_name:
+                element[
+                  'team_name'
+                ],
+              team_status:
+                element[
+                  'team_status'
+                ],
+            });
+          }
+          });
+
+          if (this.dataSource.length > 0) {
+            this.is_completedTask = true;
+          }
+          // Fire sucess toast
+          this.utilityService.fireToast(
+            'success',
+            `Data fetched successfully`
+          );
+        })
+        .catch(() => {
+          // Fire error toast
+          this.utilityService.fireToast(
+            'error',
+            `Some unexpected error occured, please try again!`
+          );
+        });
+    });
+  }
 }
