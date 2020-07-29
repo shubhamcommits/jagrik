1d0ddf506d2ea14a1a801dec706b9decc106749e
yashgupta <ygroks1998@gmail.com>
2020-07-29 11:39:02 +0530

    Submit Bonus Task Route Added

--- server/api/routes/bonus_task.routes.ts
+++ server/api/routes/bonus_task.routes.ts
@@ -1,5 +1,15 @@
-import express from 'express'
+import express, { Request } from 'express'
 import { BonusTaskController } from "../controllers";
+import multer from 'multer';
+
+// Multer Middleware
+const upload = multer({
+    dest: '../../uploads',
+    filename: (req: Request, file: any, cb: any) => {
+      const ext = file.mimetype.split('/')[1]
+      cb(null, `BonusTask-${req.headers.authorization}-${Date.now()}.${ext}`)
+    },
+  })
 
 // BonusTask Controller with all the functions
 const bonusTaskController = new BonusTaskController();
@@ -10,6 +20,8 @@ const routes = express.Router();
 routes.post('/create-bonus-task',bonusTaskController.createBonusTask);
 
 routes.post('/assign-user-bonus-task', bonusTaskController.assignUserBonusTask);
+
+routes.post('/user-submit-bonus-task',upload.single('supporting_doc'),bonusTaskController.assignUserBonusTask);
 
 routes.get('/get-class-bonus-tasks', bonusTaskController.getBonusTasks);
 
