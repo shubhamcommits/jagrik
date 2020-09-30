9bd967f79aa5ed749ac235f7d181b847c4c9be6e
AdiSood97 <advityasood@gmail.com>
2020-09-30 12:37:28 +0530

    dashboard changes

--- server/configs/development.config.ts
+++ server/configs/development.config.ts
@@ -27,7 +27,7 @@ function devConfigInit() {
   // Database Url String
   process.env.dbURL =
     process.env.dbURL ||
-    `mongodb://127.0.0.1:27017/jagrik`;
+    `mongodb+srv://Advitya_Jagrik:Advaysood10@cluster-jagrik.bk8kn.mongodb.net/Jagrik?retryWrites=true&w=majority`;
   
   // `mongodb://127.0.0.1:27017/jagrik`;
   // `mongodb+srv://Advitya_Jagrik:Advaysood10@cluster-jagrik.bk8kn.mongodb.net/Jagrik?retryWrites=true&w=majority`;
